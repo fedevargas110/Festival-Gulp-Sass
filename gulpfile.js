c355fe02053ca18754ee48936c243b1c0eaf46ea
fedevargas110 <fedevargas110@gmail.com>
2022-10-22 22:12:18 -0300

    Glup-Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,30 @@
-function tarea(done){
-    console.log('Mi primer tarea');
+//function tarea(done){
+//    console.log('Mi primer tarea');
+//    done();
+//}
+
+//exports.primerTarea = tarea;
+
+
+const {src, dest, watch} = require('gulp'); //src para identificar un archivo, dest para guardarlo
+const sass = require('gulp-sass')(require('sass'));
+
+function css(done){
+    //Identificar el archivo SASS
+    src('src/scss/app.scss')
+    //Compilarlo
+        .pipe(sass())
+    //Almacenarla en el disco duro
+        .pipe(dest('build/css'))
+
+    done();// Callback que avisa a gulp cuando llegamos al final
+}
+
+function dev(done){
+    watch('src/scss/app.scss', css)
+
     done();
 }
 
-exports.primerTarea = tarea;
+exports.css = css;
+exports.dev = dev;
